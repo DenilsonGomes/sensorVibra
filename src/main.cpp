c15511279dcc416d76002aa0c3759cdfb21876d0
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-20 11:15:12 -0300

    Codigo finalizado e comentado

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,22 @@
 #include <Arduino.h>
 
 //Variaveis e constantes
-int sensor = 2;
-int led = 3;
-int valor;
+int sensor = 2; //Pino do sensor
+int led = 3; //Pino do led
+int valor; //variavel para leitura do sensor
 
 //setup
 void setup() {
-    Serial.begin(9600);
-    pinMode(sensor, INPUT);
-    pinMode(led, OUTPUT);
+    Serial.begin(9600); //Inicia a comunicação serial
+    pinMode(sensor, INPUT); //sensor como entrada
+    pinMode(led, OUTPUT); //led como saida
 }
 
 //loop
 void loop() {
-    valor = digitalRead(sensor);
-    digitalWrite(led,valor);
-    Serial.print("O valor do sensor é: ");
-    Serial.println(valor);
-    delay(2000);
+    valor = digitalRead(sensor); //Atribui a leitura do sensor para valor
+    digitalWrite(led,valor); //Atribui o valor do sensor para saida do led
+    Serial.print("O valor do sensor é: "); //Exibe a mensagem
+    Serial.println(valor); //Exibe o valor do sensor
+    delay(2000); //Espera 2 segundos
 }
