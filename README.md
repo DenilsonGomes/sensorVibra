e6a736c85ac64d7d97e1f993df494daed92bf65d
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-20 10:48:33 -0300

    Projeto iniciado

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿***
+<h1 align="center" > Sensor Vibração Ky-002
+
+<br>
+<br>
+
+<h2 align="right" >Denilson Gomes Vaz da Silva<br>
+<br>
+
+Descrição
+==========
+
+<p>Esse projeto analisa o valor do sinal obtido atraves do sensor de vibração Ky-002.<p/>
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*Autor: Denilson Gomes Vaz da Silva
+* Graduando em Engenharia de Computação
+* Universidade Federal do Ceará - UFC
+*
+* sensorVibra: Codigo para analisar o sinal obtido do sensor de vibração Ky-002
+*/
+
+#include <Arduino.h>
+
+//Variaveis e constantes
+int sensor = 2;
+int led = 3;
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
